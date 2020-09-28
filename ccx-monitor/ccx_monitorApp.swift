32fdbe71c2fb685ea08060d82a3603420768c4cb
Jason Goodney <jason.goodney@gmail.com>
2020-09-28 09:20:24 -0400

    Initial Commit

---
+++ ccx-monitor/ccx_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ccx_monitorApp.swift
+//  ccx-monitor
+//
+//  Created by Jason Goodney on 9/28/20.
+//
+
+import SwiftUI
+
+@main
+struct ccx_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
